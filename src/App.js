6e5e95baa4ea45a907b754473145f5f6d20c23e0
Piepognwong <j.tonneyck@gmail.com>
2020-07-07 16:41:29 +0200

    no warnings

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react';
 import {Route} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Students from "./pages/Students";
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+function App (props) {
+  return (
+    <div>
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      
+      <Route path="/students" component={Students}/>
 
-
-  }
-
-  render() {
-    return (
-      <div>
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        
-        <Route path="/students" component={Students}/>
-
-      </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import SideBarLayout from "../layout/SidebarNavLayout";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 function About() {
     return (
         <SideBarLayout>
